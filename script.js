8a018e6684164e7f0bcddb95fd6d417b41077dd6
olaxkid 911 <lukmanuthmanabubakr>
2023-11-27 11:48:30 +0100

    second commit

---
+++ script.js
@@ -0,0 +1,9 @@
+let js = "amazing";
+if (js === "amazing") {
+  alert("Javascript is fun");
+  console.log("Javascript is fun");
+} else {
+  alert("javascript is not fun");
+}
+
+console.log(40 + 8 + 23 - 10);
