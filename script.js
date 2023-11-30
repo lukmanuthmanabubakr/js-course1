38bb44bce27e5285a7ad444756e4a14d70b3284a
olaxkid 911 <lukmanuthmanabubakr>
2023-11-30 15:01:54 +0100

    third commit

--- script.js
+++ script.js
@@ -1,9 +1,40 @@
 let js = "amazing";
 if (js === "amazing") {
-  alert("Javascript is fun");
+  // alert("Javascript is fun");
   console.log("Javascript is fun");
 } else {
-  alert("javascript is not fun");
+  // alert("javascript is not fun");
+  console.log("javascript is not fun");
 }
 
-console.log(40 + 8 + 23 - 10);
+console.log(40 + 40 + 30);
+
+//Values and Varialbles
+
+console.log("Abu");
+console.log(23);
+
+let firstName = "Abubakr";
+let first_name = "abubakr";
+let first = "Abubakr";
+
+console.log(firstName);
+console.log(firstName);
+
+// let 3years = 3
+
+// let jonas#hhs = "lukman Abubakr"; wrong to use !!!!
+
+// let new = 27 wrong to use !!!! same go as function, name e.g
+
+// let function = 21
+
+// let name = "ola";
+
+//do not start ur varible name with capital e.g
+
+// let Person = "Daday"; instead let person = "Person"
+
+let PI = 3.1415;
+
+let myFirstJob = "Programmer";
